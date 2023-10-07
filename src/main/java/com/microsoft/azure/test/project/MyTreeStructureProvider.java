00565b1d6e18ff18ed00e849c5f2abcb5dd0d5a5
Miller Wang <wangmi@microsoft.com>
2023-10-07 10:30:55 +0800

    temp

--- src/main/java/com/microsoft/azure/test/project/MyTreeStructureProvider.java
+++ src/main/java/com/microsoft/azure/test/project/MyTreeStructureProvider.java
@@ -2,18 +2,13 @@ package com.microsoft.azure.test.project;
 
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
-import com.azure.core.credential.TokenCredential;
-import com.azure.core.management.profile.AzureProfile;
 import com.azure.resourcemanager.resources.models.Subscription;
-import com.azure.resourcemanager.resources.models.Tenant;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
-import com.intellij.notification.NotificationGroupManager;
-import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
@@ -21,15 +16,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
-
-import static com.microsoft.azure.test.auth.AzureIdentityExample.environment;
-import static com.microsoft.azure.test.auth.AzureIdentityExample.getTenantTokenCredential;
-import static com.microsoft.azure.test.auth.AzureIdentityExample.listSubscriptions;
-import static com.microsoft.azure.test.auth.AzureIdentityExample.listTenants;
-import static com.microsoft.azure.test.auth.AzureIdentityExample.signWithOAuth;
-import static com.microsoft.azure.test.auth.AzureIdentityExample.tenantId7d;
 
 public final class MyTreeStructureProvider implements TreeStructureProvider {
     private final Project myProject;
@@ -107,22 +94,6 @@ public final class MyTreeStructureProvider implements TreeStructureProvider {
         @Override
         @NotNull
         public Collection<? extends AbstractTreeNode<?>> getChildren() {
-            final TokenCredential credential = signWithOAuth(tenantId7d);
-            List<Tenant> tenants = listTenants(credential, new AzureProfile(environment));
-            for (Tenant tenant : tenants) {
-                NotificationGroupManager.getInstance()
-                    .getNotificationGroup("Azure Notification Group")
-                    .createNotification(tenant.innerModel().displayName(), NotificationType.INFORMATION)
-                    .notify(this.getProject());
-                String tid = tenant.tenantId();
-                if (tenant.tenantId().endsWith("8c07b7d8-d6fa-4e5b-be42-c1a09b2fa19f")) {
-                    continue;
-                }
-                final TokenCredential c = getTenantTokenCredential(credential, tid);
-                AzureProfile profile = new AzureProfile(tid, null, environment);
-                final List<Subscription> subs = listSubscriptions(c, profile);
-                return new ArrayList<>(subs.stream().map(s -> new SubscriptionNode(myProject, s)).toList());
-            }
             return Collections.emptyList();
         }
 
